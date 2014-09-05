07d69be25f4ce0c5dc1e2347cf4761f78f00c062
Konstantin Alexandrov <koalexan@microsoft.com>
2014-09-05 17:50:46 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+exports.Nuve = {
+    serviceID: '540887c01e45fffae69a2dfd',
+    serviceKey: '570',
+    host: 'http://localhost:3000/',
+    roomName: 'negus-office-galaxy-room'
+};
---
+++ licode_heartbeat.js
@@ -0,0 +1,68 @@
+/*jshint curly:true, indent:4, strict:true*/
+
+var config = require('./config');
+
+var N = require('./vendor/nuve');
+var Erizo = require('./vendor/erizofc');
+
+var roomID, erizoRoom;
+
+
+N.API.init(config.Nuve.serviceID, config.Nuve.serviceKey, config.Nuve.host);
+
+var errorCallback = function (e) {
+    "use strict";
+    console.log(e);
+};
+
+var dataStream = Erizo.Stream({
+    data: true,
+    attributes: {role: 'server'},
+});
+
+var onCreateToken = function (token) {
+    "use strict";
+
+    console.log('Token created: ', token);
+    erizoRoom = Erizo.Room({token: token});
+
+    dataStream.init();
+    erizoRoom.connect();
+
+    erizoRoom.addEventListener('room-connected', function (roomEvent) {
+        console.log("Connected to the room.");
+        erizoRoom.publish(dataStream);
+
+        console.log("Data stream published.");
+        setInterval(function () {
+            dataStream.sendData({action: 'update-heartbeat'});
+            console.log("Heartbeat sent");
+        }, 5000);
+    }); 
+};
+
+var createToken = function (roomID) {
+    "use strict";
+    N.API.createToken(roomID, 'server', 'server', onCreateToken, errorCallback);
+};
+
+N.API.getRooms(function (rooms) {
+    "use strict";
+    var nuveRoom;
+
+    for (var index in rooms) {
+        if (rooms[index].name == config.Nuve.roomName) {
+            nuveRoom = rooms[index];
+        }
+    }
+
+    if (nuveRoom === undefined) {
+        nuveRoom = N.API.createRoom(config.Nuve.roomName, function(room) {
+            console.log('Room created with id: ', room._id);
+            createToken(room._id);
+        }, errorCallback);
+    } else {
+        createToken(nuveRoom._id);
+    }
+}, errorCallback);
+
